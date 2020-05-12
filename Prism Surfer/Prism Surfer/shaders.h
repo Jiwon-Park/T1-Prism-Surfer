32c7198cb19ef7629611ab771c35e29635d45b93
Seniorious <wldnjs1323@naver.com>
2020-05-12 17:48:38 +0900

    베이스는 저번 과제

---
+++ Prism Surfer/Prism Surfer/shaders.h
@@ -0,0 +1,46 @@
+#pragma once
+static const char* vert_shader = R"glsl(
+// vertex attributes
+layout(location=0) in vec3 position;
+layout(location=1) in vec3 normal;
+layout(location=2) in vec2 texcoord;
+
+// matrices
+uniform mat4 model_matrix;
+uniform mat4 view_matrix;
+uniform mat4 projection_matrix;
+
+out vec3 norm;
+out vec2 tc;
+
+void main()
+{
+	vec4 wpos = model_matrix * vec4(position,1);
+	vec4 epos = view_matrix * wpos;
+	gl_Position = projection_matrix * epos;
+
+	// pass eye-coordinate normal to fragment shader
+	norm = normalize(mat3(view_matrix*model_matrix)*normal);
+	tc = texcoord;
+}
+)glsl";
+static const char* frag_shader = R"glsl(
+#ifdef GL_ES
+	#ifndef GL_FRAGMENT_PRECISION_HIGH	// highp may not be defined
+		#define highp mediump
+	#endif
+	precision highp float; // default precision needs to be defined
+#endif
+
+// input from vertex shader
+in vec3 norm;
+in vec2 tc;
+
+// the only output variable
+out vec4 fragColor;
+
+void main()
+{
+	fragColor = vec4(tc.xy, 0, 1);
+}
+)glsl";
